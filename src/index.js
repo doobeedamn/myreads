b0454d2ecc2c4e1a4e97252130698a7adc28f053
Agnieszka Tanalska <agnieszka.tanalska@gmail.com>
2018-08-05 12:29:05 +0200

    Refactor: Completing the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { BrowserRouter, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import HomePage from "./pages/HomePage";
 import './index.css'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="app">
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/search" component={SearchPage} />
+            </div>
+        </BrowserRouter>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
